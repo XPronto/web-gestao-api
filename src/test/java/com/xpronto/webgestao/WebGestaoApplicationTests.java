2543c29fc473dd90be2c942ab7e5374b8271e20a
Lucas Suares <hub_suares.01@hotmail.com>
2025-04-06 14:25:27 -0300

    chore: base spring boot project
    In this commit, the base of the Spring Boot API was configured.

---
+++ src/test/java/com/xpronto/webgestao/WebGestaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xpronto.webgestao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebGestaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
