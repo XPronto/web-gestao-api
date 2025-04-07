e92b90618158426072c94ff4132061d65d8aacfb
Lucas Suares <hub_suares.01@hotmail.com>
2025-04-06 21:31:10 -0300

    feat: teste
    teste

--- src/main/java/com/xpronto/webgestao/WebGestaoApplication.java
+++ src/main/java/com/xpronto/webgestao/WebGestaoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebGestaoApplication {
 
 	public static void main(String[] args) {
+        System.out.println("Teste");
 		SpringApplication.run(WebGestaoApplication.class, args);
 	}
 
